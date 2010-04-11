a1cb805af0757bb360bfef66f74206589dfbf01e
Konrad Hinsen <konrad.hinsen@fastmail.net>
2010-04-11 13:32:53 +0200

    New version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject units "0.2.0"
+(defproject units "0.2.1"
   :description "Physical units for Clojure"
   :url "http://code.google.com/p/clj-units/"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
