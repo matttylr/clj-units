ccc04df08ac03fa4ee84e61fcfe0cb602ce70599
Konrad Hinsen <konrad.hinsen@fastmail.net>
2010-08-25 15:45:35 +0200

    Administrative updates - use Clojure 1.2 release - extend .hgignore - increase version number

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject units "0.2.2"
+(defproject units "0.2.3"
   :description "Physical units for Clojure"
   :url "http://code.google.com/p/clj-units/"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-		 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-		 [nstools "0.2.3"]
-		 [methods-a-la-carte "0.1.0"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]
+		 [org.clojure/clojure-contrib "1.2.0"]
+		 [nstools "0.2.4"]
+		 [methods-a-la-carte "0.1.0"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
