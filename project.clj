0ece6624daa13159990cf23cb63711f9ed52aec2
Konrad Hinsen <konrad.hinsen@fastmail.net>
2010-08-25 15:54:34 +0200

    Require methods-a-la-carte 0.1.1

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
 		 [org.clojure/clojure-contrib "1.2.0"]
 		 [nstools "0.2.4"]
-		 [methods-a-la-carte "0.1.0"]]
+		 [methods-a-la-carte "0.1.1"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
